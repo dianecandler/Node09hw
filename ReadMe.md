8fa19f6d92a02a9785fd82148ee192ba0f8c7435
Diane Candler <dianedcandler@gmail.com>
2020-06-22 15:33:40 -0500

    code works, github looks like it still does not

---
+++ ReadMe.md
@@ -0,0 +1,53 @@
+
+           
+# HWreadme09
+           
+ Created by dianecandler | https://api.github.com/users/dianecandler
+           
+ ![npm](https: //img.shields.io/npm/v/fs) ![npm](https://img.shields.io/npm/v/inquirer) ![npm](https://img.shields.io/npm/v/axios)
+           
+
+           
+ ![HWreadme09Screenshot](./path/to/file HWreadme09ScreScreenshot.PNG?raw=true "Homework 09 generate readme.md")
+           
+
+           
+
+           
+## Description
+           
+ Creating readme file using node.js
+           
+
+           
+## Table of Contents
+           
+* [Installation](#installation)
+           
+* [Usage](#Usage)
+           
+* [License](#License)
+           
+* [Installation](#installation)
+           
+*
+           
+*
+           
+*
+           
+## Installation
+           
+To install dpendencies run this command
+           
+           
+
+           
+ npm install inquirer axios
+           
+           
+           
+ <img src="https://avatars3.githubusercontent.com/u/63519355?v=4" alt="avatar" style=""/>
+           
+ If you have questions, you are welcome to
+           contactDiane Candler null.
--- index.js
+++ index.js
@@ -1,74 +1,73 @@
 // Learning Assistants or BCS couldn't fix GitHub conflicts so not pushing content.
+// npm install inquirer axios
+// node index.js
 
-// const questions = [];
-// function writeToFile (fileName, data) {}
-// function init () {}
-// init();
-
-//   * Installation
-//   * Usage
-//   * License
-//   * Contributing
-//   * Tests
-//   * Questions
-//   install - need to install npm
 // axios is API for backend like AJAX is for frontend
 // create file system that inquires the "api" and load to start
-const fs = require("fs");
-const inquirer = require("inquirer");
-const axios = require("axios");
+const fs = require('fs');
+const inquirer = require('inquirer');
+const axios = require('axios');
 
 // this should be an inquirer not require
 inquirer
-   .prompt([{
-         type: "Input",
-         name: "gitHubUsername",
-         message: "What is your GitHub username?"
-      },
-      {
-         type: "Input",
-         name: "projectName",
-         message: "What is title or name of your project?"
-      },
-      {
-         type: "Input",
-         name: "altText",
-         message: "What is the alternate text for the screenshot?"
-      },
-      {
-         type: "Input",
-         name: "description",
-         message: "Provide a short description of your project"
-      },
+	.prompt([
+		{
+			type: 'Input',
+			name: 'gitHubUsername',
+			message: 'What is your GitHub username?'
+		},
+		{
+			type: 'Input',
+			name: 'projectName',
+			message: 'What is title or name of your project?'
+		},
+		{
+			type: 'Input',
+			name: 'altText',
+			message: 'What is the alternate text for the screenshot?'
+		},
+		{
+			type: 'Input',
+			name: 'description',
+			message: 'Provide a short description of your project'
+		},
 
-      {
-         type: "Input",
-         name: "installDep",
-         message: "What command should you use to call the dependencies?",
-         default: "npm i"
-      },
-      {
-         type: "Input",
-         name: "deployedRepo",
-         message: "What is the deployed app?"
-      },
-      {
-         type: "Input",
-         name: "miscUsage",
-         message: "What does a user need to know about the repo?"
-      },
-    //   license - choices should found in GitHub https://choosealicense.com
-    {
-        type: "list",
-        name: "license",
-        message: "Please select a license for your project?",
-        choices: ["MIT", "AGPL-3.0-or-later", "LGPL-3.0-or-later", "MPL-2.0", "Apache-2.0", "BSL-1.0", "MIT","None"],
-     },
-     .then(function (response) {
-        axios
-           .get(`https://api.github.com/users/${response.gitHubUsername}`)
-           .then(function (gitRes) {
-              const md = `
+		{
+			type: 'Input',
+			name: 'installDep',
+			message: 'What command should you use to call the dependencies?',
+			default: 'npm i'
+		},
+		{
+			type: 'Input',
+			name: 'deployedRepo',
+			message: 'What is the deployed app?'
+		},
+		{
+			type: 'Input',
+			name: 'miscUsage',
+			message: 'What does a user need to know about the repo?'
+		},
+		//   license - choices should found in GitHub https://choosealicense.com
+		{
+			type: 'list',
+			name: 'license',
+			message: 'Please select a license for your project?',
+			choices: [
+				'MIT',
+				'AGPL-3.0-or-later',
+				'LGPL-3.0-or-later',
+				'MPL-2.0',
+				'Apache-2.0',
+				'BSL-1.0',
+				'MIT',
+				'None'
+			]
+		}
+	])
+	.then(function (response) {
+		axios.get(`https://api.github.com/users/${response.gitHubUsername}`).then(function (gitRes) {
+			const md = `
            \r\n# ${response.projectName}
            \r\n Created by ${gitRes.data.login} | ${gitRes.data.url}
            \r\n ![npm](https: //img.shields.io/npm/v/fs) ![npm](https://img.shields.io/npm/v/inquirer) ![npm](https://img.shields.io/npm/v/axios)
@@ -97,16 +96,16 @@ inquirer
            \r\n <img src="${gitRes.data.avatar_url}" alt="avatar" style=""/>
            \r\n If you have questions, you are welcome to
            contact${gitRes.data.name} ${gitRes.data.email}.`;
-              fs.writeFile("ReadMe.md", md, (err) => {
-                 if (err) {
-                    console.log(err);
-                 } else {
-                    console.log("ReadMe.md has been created!")
-                 }
-              });
-           });
-     })
+			fs.writeFile('ReadMe.md', md, (err) => {
+				if (err) {
+					console.log(err);
+				}
+				else {
+					console.log('ReadMe.md has been created!');
+				}
+			});
+		});
+	});
 
-// not sure how to call axios but thinking .get and .then and request response.  Need to read up.
 // remember to use \r\n for spacing and spreading text
-// need to fix GitHub.  Cannot save to Node09hw so using the test branch.  
+// need to fix GitHub.  Cannot save to Node09hw so using the test branch.  Cannot write to github.com
