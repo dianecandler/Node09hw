9652d26e41a8dcd516245236c7acbb5d0cd7bb64
Diane Candler <dianedcandler@gmail.com>
2020-06-21 21:23:35 -0500

    trouble with github, working through logic of axios

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function writeToFile (fileName, data) {}
 function init () {}
 
 init();
+
+// axios is API for backend like AJAX is for frontend
+// create file system that inquires the "api" to start
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+require
+   .prompt([{
+         type: "Input",
+         name: "gitHubUsername",
+         message: "What is your GitHub username?"
+      },
+      {
+         type: "Input",
+         name: "projectName",
+         message: "What is your project name?"
+      },
+,then(function (response){
+    axios
+        .get(`https://api.github.com/users/)
+    
+})
+
+// not sure how to call axios but thinking .get and .then and request response.  Need to read up.
+// remember to use \r\n for spacing and spreading responses
